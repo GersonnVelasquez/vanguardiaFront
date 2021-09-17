1b0f9f08114d4fb0dff400251d629dbb0aa07e35
Gersonn Velasquez <yersonn.velasquez@gmail.com>
2021-09-17 13:44:50 -0600

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  backendUrl: 'http://localhost:3900/'
+  backendUrl: 'https://vangbackend.herokuapp.com/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  backendUrl: 'http://localhost:3900/'
+  backendUrl: 'https://vangbackend.herokuapp.com/'
 };
 
 /*
